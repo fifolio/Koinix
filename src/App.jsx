b279df36430ed562f9f8a275602b8093003fd71d
Firas <firasdabbabi@gmail.com>
2023-07-10 17:17:23 +0100

    adding @Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,56 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-import Coin from './components/coin/Coin';
+// import axios from 'axios';
+// import {useState, useEffect} from 'react';
+// import Coin from './components/coin/Coin';
+import Header from './components/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App(){
 
-  const api = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+  // const api = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
 
 
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
+  // const [coins, setCoins] = useState([]);
+  // const [search, setSearch] = useState('');
 
-  useEffect(() => {
-    axios.get(api)
-    .then(res => {
-      setCoins(res.data);
-    })
-    .catch(error => console.log(error))
-  }, []);
+  // useEffect(() => {
+  //   axios.get(api)
+  //   .then(res => {
+  //     setCoins(res.data);
+  //   })
+  //   .catch(error => console.log(error))
+  // }, []);
 
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
+  // const handleChange = e => {
+  //   setSearch(e.target.value);
+  // };
 
-  const filteredCoins = coins.filter(coin => 
-    coin.name.toLowerCase().includes(search.toLowerCase()));
+  // const filteredCoins = coins.filter(coin => 
+  //   coin.name.toLowerCase().includes(search.toLowerCase()));
 
-  return (
-  <div className='coin-app'>
 
-    <div className="coin-search">
-      <h1 className='coin-text'>Search a currency</h1>
-      <form>
-        <input type="text" placeholder='Search' className='coin-input' onChange={handleChange}/>
-      </form>
-    </div>
+    return (
+      <div className='main-container container-sm'>
+      <Header />
+      </div>
+    )
 
-    {filteredCoins.map(coin => {
-      return (
-        <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} marketcap={coin.market_cap} price={coin.current_price} priceChange={coin.price_change_percentage_24h} volume={coin.total_volume}/>
-      )
-    })}
-  </div>
-  )
+  // return (
+  // <div className='coin-app'>
+
+  //   <div className="coin-search">
+  //     <h1 className='coin-text'>Search a currency</h1>
+  //     <form>
+  //       <input type="text" placeholder='Search' className='coin-input' onChange={handleChange}/>
+  //     </form>
+  //   </div>
+
+  //   {filteredCoins.map(coin => {
+  //     return (
+  //       <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} marketcap={coin.market_cap} price={coin.current_price} priceChange={coin.price_change_percentage_24h} volume={coin.total_volume}/>
+  //     )
+  //   })}
+  // </div>
+  // )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/no-unknown-property */
+import {BsGithub} from 'react-icons/bs';
+
+export default function Header() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-dark navbar-dark mt-3">
+                <div className="container-sm">
+                    <a className="navbar-brand">
+                        <img src="https://icon-library.com/images/bitcoin-icon-png/bitcoin-icon-png-21.jpg" width="35" height="35" />
+                        <span className="navbar-brand-name">
+                            Koinix
+                        </span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link">|  Providing a fundamental analysis of the crypto market</a>
+                            </li>
+
+                        </ul>
+                        <form className="d-flex">
+                            <a href="https://github.com/fifolio/Koinix" target="_blank" style={{fontWeight: '500'}} className="btn btn-outline-light text-bold " type="submit"><BsGithub className='react-icon'/> Github Repository</a>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
